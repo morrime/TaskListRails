7788096e499d1df171f2bfe95395877d8021f8f3
Mary Morrison <marymorrison15@gmail.com>
2016-10-24 21:41:25 -0700

    oops, forgot commits until now. Added sessions controller actions create, login, show, and logout. Also, private application controller actions logged_in? and current_user

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,11 @@ Rails.application.routes.draw do
   get '/tasks/:id/complete' => 'tasks#update'
 
 
+  get 'auth/github/callback' => 'sessions#create'
+  get '/sessions/login_failure', to: 'sessions#login_failure', as: 'login_failure'
+  get '/sessions', to: 'sessions#index', as: 'sessions'
 
-
+  delete '/sessions', to: 'sessions#destroy'
 
 
   # Example of regular route:
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+  skip_before_action :logged_in?, only: [:login, :create]
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+    redirect_to login_path unless auth_hash[:uid]
+    @user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if @user.nil?
+      @user = User.create_from_github(auth_hash)
+      redirect_to login_path unless @user.save
+    end
+    session[:user_id] = auth_hash[:uid]
+    redirect_to root_path
+  end
+
+  def login; end
+
+  def show
+    redirect_to login_path
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end

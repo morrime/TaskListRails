cd716668727f78a851f9db498b6ecc90c0d9bf5d
Mary Morrison <marymorrison15@gmail.com>
2016-10-25 22:01:20 -0700

    refactored actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :require_login
 
   private
 
-   def current_user
-     begin
-       @current_user ||= User.find(session[:user_id]) if session[:user_id]
-     rescue ActiveRecord::RecordNotFound
-       nil
-     end
-   end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-
-   def require_login
-      redirect_to login_path if session[:user_id].nil?
+  def require_login
+    if current_user.nil?
+     redirect_to login_path
     end
-  #  def require_login
-  #    if current_user.nil?
-  #      flash[:error] = "You must be logged in to view this section"
-  #      redirect_to session_path
-  #    end
-  #  end
+  end
 end

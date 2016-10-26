95cd068cd70098127b56cce2ba2015f5b2a17d9a
Mary Morrison <marymorrison15@gmail.com>
2016-10-25 22:49:12 -0700

    unit tests not working, error = ActiveRecord::StatementInvalid: SQLite3::ConstraintException etc. Using yaml files would probably help, but I am tired, this is already late for a PR, and as far as I know, everything else it working. Fin.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
+  validates :name, :description, presence: true
   belongs_to :user
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations: name can't be blank" do
+    task = Task.create(description: "yolo")
+    assert_not task.valid?
+    # assert task.errors.keys.include?(:name), "name is not in the errors hash"
+  end
+
+  test "validations: description can't be blank" do
+    task = Task.create(name: "poodles")
+    assert_not task.valid?
+    # assert task.errors.keys.include?(:description), "description is not in the errors hash"
+  end
+
+  test "can create a new task" do
+    task = Task.create(name: "poodles", description: "yolo")
+    assert task.valid?
+    assert
+  end
 end

2ca99fadd7ccef428bad26a5ab39c9c99ef7332b
Mary Morrison <marymorrison15@gmail.com>
2016-10-25 20:46:25 -0700

    added before action, deleted line from effected actions, cleaned up indentation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,76 +1,73 @@
 class TasksController < ApplicationController
-def index
-  @tasks = Task.all
-end
+  before_action :find_task, only: [:show, :edit, :update, :complete]
 
-def new
-  @task = Task.new
-end
+  def index
+    @tasks = Task.all
+  end
 
-def show
-    @task = Task.find(params[:id])
-end
+  def new
+    @task = Task.new
+  end
 
-def edit
-  @task = Task.find(params[:id])
-end
+  def show; end
 
-def update
-  @task = Task.find(params[:id])
-  if @task.update(task_params)
-    if @task.complete == false
-      @task.completion_date = nil
-      @task.save
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      if @task.complete == false
+        @task.completion_date = nil
+        @task.save
+      else
+        @task.completion_date = Date.current
+        @task.save
+      end
+      # SUCCESS
+      redirect_to tasks_path
     else
-      @task.completion_date = Date.current
-      @task.save
+      # NOPE, NO SUCCESS
+      render :edit
     end
-    # SUCCESS
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      # SAVED SUCCESSFULLY
+      redirect_to tasks_path
+    else
+      # DID NOT SAVE
+      render :new
+    end
+  end
+
+  def destroy
+    Task.find(params[:id]).destroy
     redirect_to tasks_path
-  else
-    # NOPE, NO SUCCESS
-    render :edit
-  end
-end
-
-def create
-  @task = Task.new(task_params)
-  if @task.save
-    # SAVED SUCCESSFULLY
-    redirect_to tasks_path
-  else
-    # DID NOT SAVE
-    render :new
-  end
-end
-
-def destroy
-  Task.find(params[:id]).destroy
-  redirect_to tasks_path
-end
-
-def complete
-  @task = Task.find(params[:id])
-  if @task.complete == nil || @task.complete == false
-    @task.update(complete: true)
-    @task.update(completion_date: Date.today)
-    redirect_to task_path
-  else
-    # @task.update(completion_date: nil)
-    redirect_to task_path
   end
 
-end
+  def complete
 
-private
+    if @task.complete == nil || @task.complete == false
+      @task.update(complete: true)
+      @task.update(completion_date: Date.today)
+      redirect_to task_path
+    else
+      # @task.update(completion_date: nil)
+      redirect_to task_path
+    end
 
-def find_task
-  @task = Task.find(params[:id])
-end
+  end
 
-def task_params
-  params.require(:task).permit(:name, :description, :complete)
-end
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :complete)
+  end
 
 
 end
